f3e31a2e0082c479710d92dcd2127ed2feea8f04
Ayush Kumar Sharma <47772616+ayuu5h@users.noreply.github.com>
2020-03-26 10:49:32 +0530

    Update gym.py

--- gym.py
+++ gym.py
@@ -27,4 +27,19 @@ class Environment(gym.Env):
         Parameter('sad_num_epochs', min_value=5, max_value=30),
     ]
 
+    #https://hackernoon.com/intuitive-rl-intro-to-advantage-actor-critic-a2c-4ff545978752
+    Environment.params += [
+            Parameter('_channel_%d' % ch, min_value=0, max_value=1, meta=ch + 1) for ch in
+            range(self._histogram_size) if ch + 1 in self._supported_channels
+        ]
+
+        self.last = {
+            'reward': 0.0,
+            'observation': None,
+            'policy': None,
+            'params': {},
+            'state': None,
+            'state_v': None
+        }
+
    
