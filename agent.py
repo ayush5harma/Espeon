11592146fc572eb60132dfb34356976bb3028c17
Ayush Sharma <47772616+ayuu5h@users.noreply.github.com>
2020-03-29 21:30:57 +0530

    Added Agent's Pseudocode

--- agent.py
+++ agent.py
@@ -1,3 +1,18 @@
+# Agent's pseudocode 
+## main loop
+# while True:
+    ## ask bettercap for all visible access points and their clients
+ #   aps = get_all_visible_access_points()
+    ## loop each AP
+  #  for ap in aps:
+        ## send an association frame in order to grab the PMKID
+   #     send_assoc(ap)
+        ## loop each client station of the AP
+    #    for client in ap.clients:
+            ## deauthenticate the client to get its half or full handshake
+     #       deauthenticate(client)
+
+    # wait_for_loot()
 
 
 
