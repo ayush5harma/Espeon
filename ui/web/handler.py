0b82939361575a4f96eaf9fd1fb06ddd492de582
ayuu5h <robust.ayush@gmail.com>
2020-04-14 23:35:45 +0530

    web-view-handler

--- ui/web/handler.py
+++ ui/web/handler.py
@@ -1,1 +1,59 @@
+class Handler:
+    def __init__(self, config, agent, app):
+        self._config = config
+        self._agent = agent
+        self._app = app
 
+        self._app.add_url_rule('/ui', 'ui', self.with_auth(self.ui))
+
+        self._app.add_url_rule('/shutdown', 'shutdown', self.with_auth(self.shutdown),    methods=['POST'])
+        self._app.add_url_rule('/reboot', 'reboot', self.with_auth(self.reboot), methods=['POST'])
+        self._app.add_url_rule('/restart', 'restart', self.with_auth(self.restart), methods=['POST'])
+
+    def _check_creds(self, u, p):
+        # trying to be timing attack safe
+        return secrets.compare_digest(u, self._config['username']) and \
+               secrets.compare_digest(p, self._config['password'])
+
+    def with_auth(self, f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            auth = request.authorization
+            if not auth or not auth.username or not auth.password or not self._check_creds(auth.username,
+                                                                                           auth.password):
+                return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Unauthorized"'})
+            return f(*args, **kwargs)
+
+        return wrapper
+ # serve a message and shuts down the unit
+    def shutdown(self):
+        try:
+            return render_template('status.html', title=pwnagotchi.name(), go_back_after=60,
+                                   message='Shutting down ...')
+        finally:
+            _thread.start_new_thread(pwnagotchi.shutdown, ())
+
+    # serve a message and reboot the unit
+    def reboot(self):
+          try:
+              return render_template('status.html', title=pwnagotchi.name(), go_back_after=60,
+                                     message='Rebooting ...')
+          finally:
+              _thread.start_new_thread(pwnagotchi.reboot, ())
+
+    # serve a message and restart the unit in the other mode
+    def restart(self):
+        mode = request.form['mode']
+        if mode not in ('AUTO', 'MANU'):
+            mode = 'MANU'
+
+        try:
+            return render_template('status.html', title=pwnagotchi.name(), go_back_after=30,
+                                   message='Restarting in %s mode ...' % mode)
+        finally:
+            _thread.start_new_thread(pwnagotchi.restart, (mode,))
+
+    # serve the PNG file with the display image
+    def ui(self):
+        with web.frame_lock:
+            return send_file(web.frame_path, mimetype='image/png')        
